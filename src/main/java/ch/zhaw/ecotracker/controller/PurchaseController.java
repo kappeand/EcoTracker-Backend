237b5966f75db1155fb4ce1eb83784f6928ae0d5
Andrin Kappeler <kappeand@students.zhaw.ch>
2021-12-30 11:59:30 +0100

    fixed camelcase of request urls

--- src/main/java/ch/zhaw/ecotracker/controller/PurchaseController.java
+++ src/main/java/ch/zhaw/ecotracker/controller/PurchaseController.java
@@ -23,7 +23,7 @@ public class PurchaseController extends BaseController<Purchase> {
         this.purchaseRepository = purchaseRepository;
     }
 
-    @GetMapping(value = "/customer/{customerId}")
+    @GetMapping(value = "/customerId/{customerId}")
     public ResponseEntity<List<Purchase>> readByCustomerId(@PathVariable Long customerId) {
         return new ResponseEntity<>(this.purchaseRepository.findByCustomerId(customerId), HttpStatus.OK);
     }
